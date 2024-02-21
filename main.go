1873ef7b0d2c391a8dc36c2a62b5d1a2a1ac89ba
Flavio H. Galon <flaviogalon@gmail.com>
2024-02-21 20:02:10 +0000

    Getting started with the web server

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+// Temporary home handler
+func home(w http.ResponseWriter, e *http.Request) {
+	w.Write([]byte("Hello from snippetbox!"))
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	mux := http.NewServeMux()
+	// `/` is a catch-all URL pattern, so all requests will be routed to it
+	mux.HandleFunc("/", home)
+
+	log.Print("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
