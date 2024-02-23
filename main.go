b8a3c85ac48eed63e04f1061e30bf1c48c2257df
Flavio H. Galon <flaviogalon@gmail.com>
2024-02-23 19:40:40 +0000

    Handle query strings in /snippet/view

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 // Home handler. Since `/` is a catch all handler, any other unmapped route will
@@ -18,14 +20,21 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 // Display a single snippet handler
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet."))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Display a specific snippet with ID %d", id)
 }
 
 // Create a snippet handler
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		// Order here matters
-		w.Header().Set("Allow", "POST")
+		w.Header().Set("Allow", http.MethodPost)
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
